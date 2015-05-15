312cf7aea98b511000c3f616429c0f907e9147de
Akshat Mahajan <akshatm.bkk@gmail.com>
2015-05-15 01:05:46 -0700

    Added SoupStrainer object to significantly reduce document parsing time

--- scraper.py
+++ scraper.py
@@ -1,22 +1,21 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 from pandas import DataFrame as df
 
 def get_table_from(url):
 
-    '''Returns a BeautifulSoup object containing all the 'tr' tags on the requested URL.
+    '''Returns a BeautifulSoup ResultSet object containing all the 'tr' tags on the requested URL.
 
     Does some simple checking to ensure provided URL is in fact valid for BoxOfficeMojo only.''' 
 
-    identifier = '#dcdcdc'
-
     if 'boxofficemojo.com' in url:
-        response = requests.get(url)
-        raw_tables = BeautifulSoup(response.text).find_all(lambda tag: tag.name == "table" and
-                                                       tag.tr.has_attr('bgcolor') and
-                                                       tag.tr['bgcolor'] == identifier)
-        if len(table) > 1:
-            print('More than one table found - please inspect for whichever is appropriate')
+        
+        identifier = '#dcdcdc'
+        strainer = BeautifulSoup(requests.get(url).text,parse_only=SoupStrainer("table"))
+        raw_tables = strainer.find_all(lambda tag: tag.name == 'table' and
+                                       tag.tr.has_attr('bgcolor') and
+                                       tag.tr['bgcolor'] == identifier)
         return raw_tables
+
     else:
         raise ValueError('Provided URL is not a boxofficemojo.com URL')
